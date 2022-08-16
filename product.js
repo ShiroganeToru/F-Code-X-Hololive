acc88157bbe5dd188571b3cd100ac0781a78ccab
ngockim109 <ngockim9047@gmail.com>
2022-08-16 22:15:05 +0700

    update product page

--- product.js
+++ product.js
@@ -17,12 +17,12 @@
             slidesPerView: 4,
         },
     },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev"
+    // },
 });
