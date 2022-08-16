78db5ae18dd9c07ce128eaab51ad89fd8803193b
LongK17 <c4phanlong@gmail.com>
2022-08-16 23:46:37 +0700

    first

---
+++ Code/Product/product.js
@@ -0,0 +1,28 @@
+  var swiper = new Swiper(".product__list-videos", {
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    centeredSlide: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        885: {
+            slidesPerView: 3,
+        },
+        1140: {
+            slidesPerView: 4,
+        },
+    },
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev"
+    // },
+});
